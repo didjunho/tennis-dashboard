0d4f8b2ffc500967c73858d6741914b392e5563e
junho <junhopark2001@gmail.com>
2026-04-10 10:23:19 -0400

    Fix card interactivity: button elements + force SW update (v3)

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Cache is versioned: bump CACHE_VERSION after any deploy to force refresh.
  */
 
-const CACHE_VERSION = 'tennis-v2';
+const CACHE_VERSION = 'tennis-v3';
 const APP_SHELL = [
   '.',
   './index.html',
